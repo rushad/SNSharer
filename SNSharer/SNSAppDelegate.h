369823c9b86e142915166764e017efa49efa6147
Rushad <rushad@ephubudw0262t1.budapest.epam.com>
2014-06-17 19:15:10 +0400

    Initial Commit

---
+++ SNSharer/SNSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SNSAppDelegate.h
+//  SNSharer
+//
+//  Created by Rushad on 6/17/14.
+//  Copyright (c) 2014 Rushad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SNSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SNSharer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SNSharer
+//
+//  Created by Rushad on 6/17/14.
+//  Copyright (c) 2014 Rushad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SNSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([SNSAppDelegate class]));
+  }
+}
